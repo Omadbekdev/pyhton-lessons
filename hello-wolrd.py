45173ba843ccbb4c296f8ccdba8659c48cf127d9
Omadbekdev <omadbe9671@gmail.com>
2023-05-16 17:37:47 +0400

    1-lesson uploaded
    This is first lesson.

---
+++ hello-wolrd.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 21 12:34:23 2023
+
+@author: pc
+"""
+# Mavzu: print()funksiyasi.
+# Sana: 21/03/2023
+# Muallif: Omadbek
+print("Hello world!")
+
+print('Assalomu alaykum')
+print("Hayrli tong")
+print("Men 'MacBook Pro 13' modelidagi noutbuk harid qildim.")
+print("""Odami ersang, demagil odami, 
+Oniki,yo'q xalq g'amidin g'ami""")
+print('''Har kishi vafo qilsa,
+Vafo topg'usidir...''')
+print("Har kimki jafo qilsa, \nJafo topg'usidir...")
+print('Har kimki yomon bo\'lsa, \nJazo topg\'usidir')
+
+print(2+4*2)
+print(2**4)
+print(19/5)
+print(20/4)
+print(36/6)
+print(45//5)
+print(90//8)
+print(90/8)
+
+print("Beshni sakkizga qo'shsak", 5+8, "bo'ladi")
+print("Yigirmadan sakkizni ayirsak",20-8, "qoladi")
+print('5*7=',5*7,)
+print('87/8=', 87/8,)
+print("Assalomu alaykum")#Ushbu qatordagi so'zlar matnning eng yuqori qismiga joylashtiriladi.
+print("Islom olamida \"Makka\", \"Madina\" va \"Quddus\" shaharlarining o'rni yuksakdir")
+print('"Nexia","Tico",\'Damas\' ko\'rganlar qilar havas')
+print("Yigirma ikkini to'rtga bo'lganda",22/4, "qoladi")
